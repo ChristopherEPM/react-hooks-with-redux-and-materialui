d7aed7fc8d26d3ab97d4bd497b7b79a696a9ccdb
ChristopherEMP <bloodaxe00@hotmail.com>
2019-10-24 16:59:58 +0200

    created Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,42 @@
-import React from  'react';
-import { Container, Typography } from '@material-ui/core';
-export default () => (<Container><Typography variant="h4">Home</Typography></Container>)
+import React, {useState} from  'react';
+import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core';
+export default () => {
+  const [searchText, setSearchText] = useState('');
+  const handleSearchTextChange = event => {
+    setSearchText(event.target.value)
+  };
+
+  const handleCleanTextClick = event => {
+    console.log(1)
+  };
+
+  const handleSearchTextClick = event => {
+    console.log(2)
+  };
+
+  
+  return (
+    <Container>
+      <Card>
+        <Grid>
+          <Typography> Bienvenido </Typography>
+        </Grid>
+        <Grid>
+          <label>icono</label> 
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          onChange={handleSearchTextChange}/>
+        <Grid>
+        <Button variant="contained" onClick={handleCleanTextClick}> Limpiar </Button>
+        <Button variante="contained" color="primary" size="large" 
+          onClick={handleSearchTextClick}> 
+          Buscar
+        </Button>
+        </Grid>
+      </Card>
+    </Container>
+    )
+
+}
